d06bdd4088651d1644cd105c1eaf7ac93d6b4499
danieldaq <daquigan.daniel@gmail.com>
2015-05-07 20:39:17 -0700

    Initial Commit

--- mouse_a_sketch/base.js
+++ mouse_a_sketch/base.js
@@ -1,5 +1,15 @@
 console.log("this is a sanity check");
 
 $(document).ready(function(){
-  console.log("Let's get coding!")
+	console.log("Let's get coding!")
+
+	function colorMe() {
+		var color = $("input").val();
+		$(this).css("background", color);
+		
+	}
+
+	$("td").mouseover(colorMe);
+
 })
+
